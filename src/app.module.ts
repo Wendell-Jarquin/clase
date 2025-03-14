ac9a83d3eb247ac45df1326cfffcf246232830ee
Wendell_Jarquin <142639861+Wendell-Jarquin@users.noreply.github.com>
2025-03-14 23:20:01 +0100

    modulo brands

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsController } from './modules/cars/controllers/cars.controller';
 import { CarsService } from './modules/cars/services/cars.service';
+import { BrandsModule } from './modules/brands/brands.module';
 
 @Module({
-  imports: [],
+  imports: [BrandsModule],
   controllers: [AppController, CarsController],
   providers: [AppService, CarsService],
 })
